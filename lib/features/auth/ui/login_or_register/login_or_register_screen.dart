d6a9f4c95d40370e9e9e37461bac7c7636148eca
Kassem Diaa <kassemdiaa537@gmail.com>
2026-03-02 22:58:26 +0200

    makes custom auth button

--- lib/features/auth/ui/login_or_register/login_or_register_screen.dart
+++ lib/features/auth/ui/login_or_register/login_or_register_screen.dart
@@ -1,6 +1,7 @@
 import 'package:fitness_app/core/RoutesManager.dart';
 import 'package:fitness_app/core/colors_manager.dart';
 import 'package:fitness_app/core/fonts_manager.dart';
+import 'package:fitness_app/features/auth/ui/widgets/custom_auth_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -16,53 +17,22 @@ class LoginOrRegisterScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset('assets/images/logo.png',width: 100.w,),
-            SizedBox(height: 30.h,),
-            Image.asset('assets/images/start.png',width: 300.w,),
-            SizedBox(height: 60.h,),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, RoutesManager.loginScreen);
-              },
-              
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(horizontal: 150.w,vertical: 20.h),
-                backgroundColor: ColorsManager.lightPurple,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadiusGeometry.circular(27.5.r),
-                ),
-                
-              ),
-        
-              child: Text(
-                'Login',
-                style: GoogleFonts.montserratAlternates(
-                  color: ColorsManager.white,
-                  fontSize: 15.sp,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
+            Image.asset('assets/images/logo.png', width: 100.w),
+            SizedBox(height: 30.h),
+            Image.asset('assets/images/start.png', width: 300.w),
+            SizedBox(height: 60.h),
+            CustomAuthButton(
+              bgColor: ColorsManager.lightPurple,
+              fgColor: ColorsManager.white,
+              text: 'Login',
+              route: RoutesManager.loginScreen,
             ),
-            SizedBox(height: 20.h,),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, RoutesManager.registerScreen);
-              },
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(horizontal: 140.w,vertical: 20.h),
-                backgroundColor: ColorsManager.white,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadiusGeometry.circular(27.5.r),
-                ),
-              ),
-              child: Text(
-                'Register',
-                style: GoogleFonts.montserratAlternates(
-                  color: ColorsManager.black,
-                  fontSize: 15.sp,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
+            SizedBox(height: 20.h),
+            CustomAuthButton(
+              bgColor: ColorsManager.white,
+              fgColor: ColorsManager.black,
+              text: 'Register',
+              route: RoutesManager.registerScreen,
             ),
           ],
         ),
