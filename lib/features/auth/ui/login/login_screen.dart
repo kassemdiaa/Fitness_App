1158253294a2d0d026bd6a43925a04df225d893e
Kassem Diaa <kassemdiaa537@gmail.com>
2026-03-02 23:12:54 +0200

    some edits in home screen

--- lib/features/auth/ui/login/login_screen.dart
+++ lib/features/auth/ui/login/login_screen.dart
@@ -1,10 +1,52 @@
+import 'package:fitness_app/core/RoutesManager.dart';
+import 'package:fitness_app/core/colors_manager.dart';
+import 'package:fitness_app/features/auth/ui/widgets/custom_auth_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: ColorsManager.Purple,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Image.asset('assets/images/logo.png', width: 50.w),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Image.asset('assets/images/loginwelcome.png', width: 300.w),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                'forget password?',
+                style: GoogleFonts.montserratAlternates(
+                  color: ColorsManager.white,
+                  fontSize: 12.sp,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+          CustomAuthButton(
+            bgColor: ColorsManager.white,
+            fgColor: ColorsManager.black,
+            text: 'Login',
+            route: RoutesManager.layoutScreen,
+          ),
+        ],
+      ),
+    );
   }
 }
