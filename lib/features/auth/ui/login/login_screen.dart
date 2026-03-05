c34412ec1c14997b2ece4aa06891a7e9e3193f8d
Youssef Fathy <fathyyousef896@gmail.com>
2026-03-05 03:28:37 +0200

    Login overflow bug done Alhamdulillah

--- lib/features/auth/ui/login/login_screen.dart
+++ lib/features/auth/ui/login/login_screen.dart
@@ -19,120 +19,117 @@ class LoginScreen extends StatelessWidget {
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        leading: SizedBox(
-          width: 44.w,
-          height: 44.h,
-          child: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: SingleChildScrollView(
-              child: Container(
-                width: 44.w,
-                height: 44.h,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(13.r),
-                    topRight: Radius.circular(13.r),
-                    bottomLeft: Radius.circular(13.r),
-                    bottomRight: Radius.circular(13.r),
-                  ),
-                  border: Border.all(
-                    color: ColorsManager.white,
-                    width: 1.w,
-                    strokeAlign: BorderSide.strokeAlignInside,
-                  ),
-                ),
-                child: Icon(
-                  CupertinoIcons.arrow_left,
-                  color: ColorsManager.white,
-                  weight: 7.w,
-                ),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Container(
+            width: 44.w,
+            height: 44.h,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(13.r),
+                topRight: Radius.circular(13.r),
+                bottomLeft: Radius.circular(13.r),
+                bottomRight: Radius.circular(13.r),
               ),
+              border: Border.all(
+                color: ColorsManager.white,
+                width: 1.w,
+                strokeAlign: BorderSide.strokeAlignInside,
+              ),
+            ),
+            child: Icon(
+              CupertinoIcons.arrow_left,
+              color: ColorsManager.white,
+              weight: 7.w,
             ),
           ),
         ),
       ),
-      body: Container(
-        margin: EdgeInsets.symmetric(horizontal: 16.w),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [Image.asset('assets/images/logo.png', width: 50.w)],
-            ),
-            SizedBox(height: 26.h),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Image.asset('assets/images/loginwelcome.png', width: 300.w),
-              ],
-            ),
-            SizedBox(height: 26.h),
-            Custometextforemfield(
-              hint: 'Enter your Email',
-              label: 'Email',
-              color: ColorsManager.white,
-            ),
-            SizedBox(height: 16.h),
-            Custometextforemfield(
-              hint: 'Enter your Password',
-              label: 'Password',
-              color: ColorsManager.white,
-              prefixIcon: CupertinoIcons.eye,
-            ),
-            SizedBox(height: 16.h),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                InkWell(
-                  onTap: () {
-                    Navigator.pushNamed(context, RoutesManager.forgetPass);
-                  },
-                  child: Text(
-                    'forget password?',
-                    style: GoogleFonts.montserratAlternates(
-                      color: ColorsManager.white,
-                      fontSize: 12.sp,
-                      fontWeight: FontWeight.w500,
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 16.w),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 80.h,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [Image.asset('assets/images/logo.png', width: 50.w,height: 60,)],
+              ),
+              SizedBox(height: 26.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Image.asset('assets/images/loginwelcome.png', width: 300.w),
+                ],
+              ),
+              SizedBox(height: 26.h),
+              Custometextforemfield(
+                hint: 'Enter your Email',
+                label: 'Email',
+                color: ColorsManager.white,
+              ),
+              SizedBox(height: 16.h),
+              Custometextforemfield(
+                hint: 'Enter your Password',
+                label: 'Password',
+                color: ColorsManager.white,
+                prefixIcon: CupertinoIcons.eye,
+              ),
+              SizedBox(height: 16.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Navigator.pushNamed(context, RoutesManager.forgetPass);
+                    },
+                    child: Text(
+                      'forget password?',
+                      style: GoogleFonts.montserratAlternates(
+                        color: ColorsManager.white,
+                        fontSize: 12.sp,
+                        fontWeight: FontWeight.w500,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 16.h),
-            CustomElevatedButton(
-              backgroundColor: ColorsManager.white,
-              foregroundColor: ColorsManager.black,
-              onPressed: () {
-                Navigator.pushNamed(context, RoutesManager.loginScreenSucessfully);
-              },
-              title: 'Login',
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Don’t have an account?",
-                  style: FontsStyles.Entermail.copyWith(
-                    color: ColorsManager.white,
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, RoutesManager.registerScreen);
-                  },
-                  child: Text(
-                    "Register Now",
+                ],
+              ),
+              SizedBox(height: 16.h),
+              CustomElevatedButton(
+                backgroundColor: ColorsManager.white,
+                foregroundColor: ColorsManager.black,
+                onPressed: () {
+                  Navigator.pushNamed(context, RoutesManager.loginScreenSucessfully);
+                },
+                title: 'Login',
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don’t have an account?",
                     style: FontsStyles.Entermail.copyWith(
-                      color: ColorsManager.Lemon,
+                      color: ColorsManager.white,
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, RoutesManager.registerScreen);
+                    },
+                    child: Text(
+                      "Register Now",
+                      style: FontsStyles.Entermail.copyWith(
+                        color: ColorsManager.Lemon,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
